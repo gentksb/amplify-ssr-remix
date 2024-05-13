fbf0774abd02d5683789bef34f59fa7583ebb312
takagen <takagen@amazon.co.jp>
2024-05-13 16:40:11 +0900

    edit

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -5,9 +5,7 @@ rm -rf ./.amplify-hosting
 mkdir -p ./.amplify-hosting/compute
 
 cp -r ./build/server ./.amplify-hosting/compute/default
-cp package.json ./.amplify-hosting/compute/default
-cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
-
 cp -r ./build/client ./.amplify-hosting/static
+cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
