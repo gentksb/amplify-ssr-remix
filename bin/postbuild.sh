454ba6b1c2171a1393a7bf677e7c7144ebd96508
takagen <takagen@amazon.co.jp>
2024-05-13 16:00:48 +0900

    build全体をバンドルに含める

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -4,9 +4,9 @@ rm -rf ./.amplify-hosting
 
 mkdir -p ./.amplify-hosting/compute
 
-cp -r ./build/server ./.amplify-hosting/compute/default
+cp -r ./build ./.amplify-hosting/compute/default
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 
-cp -r ./build/client ./.amplify-hosting/static
+cp -r public ./.amplify-hosting/static
 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
