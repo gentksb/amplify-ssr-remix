bbbf106abb67e05cf6b42017eb7d749a645013e1
takagen <takagen@amazon.co.jp>
2024-05-13 17:01:35 +0900

    express仕様

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -2,9 +2,10 @@
 
 rm -rf ./.amplify-hosting
 
-mkdir -p ./.amplify-hosting/compute
+mkdir -p ./.amplify-hosting/compute/default/build/server
 
-cp -r ./build/server ./.amplify-hosting/compute/default
+cp -r ./build/server ./.amplify-hosting/compute/default/build
+cp -r server.js ./.amplify-hosting/compute/default/server.js
 cp -r ./build/client ./.amplify-hosting/static
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 
